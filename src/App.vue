163f264b6d554feb24d2e8d2e51750b5a1608377
dmszoltysek <dm.szoltysek@gmail.com>
2022-05-28 13:45:17 +0200

    Logowanie do systemu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <h1>Twój e-mail to {{ email }}</h1>
-    <input type="email" v-model="email">
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+
+    <div v-if="!loggedIn">
+      <label>Zaloguj się e-mailem</label>
+      <input type="text" v-model="email">
+      <button @click="logMeIn()">Wchodzę</button>
+    </div>
+  <div v-else>
+    <h2>Witaj {{email}}</h2>
+    <a @click="logOut()">Wyloguj</a>
+  </div>
   </div>
 </template>
 
@@ -10,13 +19,23 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      loggedIn: false,
     };
+  },
+  methods: {
+    logMeIn() {
+      this.loggedIn = true;
+    },
+    logOut() {
+      this.loggedIn = false;
+      this.email = '';
+    }
   }
-
 }
 </script>
 
 <style>
-
+.red{
+  color: red;
+}
 </style>
